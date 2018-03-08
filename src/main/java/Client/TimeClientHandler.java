82370bfad44f7e91d279b622acef2d0f07f8a179
unknown <songyisong@dangdang.com>
2018-03-08 20:42:34 +0800

    TCP粘包场景复现，大概率三四个时间请求同时发送给服务器，导致当成错误请求处理了

--- src/main/java/Client/TimeClientHandler.java
+++ src/main/java/Client/TimeClientHandler.java
@@ -8,28 +8,36 @@ import io.netty.channel.ChannelHandlerContext;
 /**
  * Created by songyisong on 2018/2/26.
  */
-public class TimeClientHandler extends ChannelHandlerAdapter{
-    private final ByteBuf firstMessage;
-    public TimeClientHandler(){
-        byte[] req="QUERY TIME ORDER".getBytes();
-        firstMessage= Unpooled.buffer(req.length);
-        firstMessage.writeBytes(req);
+public class TimeClientHandler extends ChannelHandlerAdapter {
+    private byte[] req;
+    private int counter;
+
+    public TimeClientHandler() {
+        req = ("QUERY TIME ORDER" + System.getProperty("line.separator")).getBytes();
     }
+
     @Override
-    public void channelActive(ChannelHandlerContext ctx)throws Exception{  //客户端和服务器端建立连接后，netty的NIO线程调用此方法
-        ctx.writeAndFlush(firstMessage);   //将消息发送给服务端
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {  //客户端和服务器端建立连接后，netty的NIO线程调用此方法
+        ByteBuf message = null;
+        for (int i = 0; i < 100; i++) {
+            message = Unpooled.buffer(req.length);
+            message.writeBytes(req);
+            ctx.writeAndFlush(message);//将消息发送给服务端
+        }
     }
+
     @Override
-    public void channelRead(ChannelHandlerContext ctx,Object mes)throws Exception{   //服务器返回消息时被调用
-        ByteBuf buf =(ByteBuf) mes;
-        byte[] req=new byte[buf.readableBytes()];
+    public void channelRead(ChannelHandlerContext ctx, Object mes) throws Exception {   //服务器返回消息时被调用
+        ByteBuf buf = (ByteBuf) mes;
+        byte[] req = new byte[buf.readableBytes()];
         buf.readBytes(req);
-        String body=new String(req,"UTF-8");
-        System.out.println("Now is : "+body);
+        String body = new String(req, "UTF-8");
+        System.out.println("Now is : " + body + " ; the counter is : " + ++counter);
     }
+
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx,Throwable cause){
-        System.out.println("Unexcepected from downstream : "+cause.getMessage());
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        System.out.println("Unexcepected from downstream : " + cause.getMessage());
         ctx.close();
     }
 }
--- src/main/java/Server/TimeServerHandler.java
+++ src/main/java/Server/TimeServerHandler.java
@@ -9,14 +9,17 @@ import io.netty.channel.ChannelHandlerContext;
  * Created by songyisong on 2018/2/26.
  */
 public class TimeServerHandler extends ChannelHandlerAdapter {
+    private int counter;
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf buf = (ByteBuf) msg;
         byte[] req = new byte[buf.readableBytes()];   //获取缓冲区可读的字节数，根据可读的字节数创建byte数组
         buf.readBytes(req);   //将缓冲区的字节数组复制到新建的byte数组中
-        String body = new String(req, "UTF-8");   //获取请求消息
-        System.out.println("TimeServer Receive order : " + body);
+        String body = new String(req, "UTF-8").substring(0, req.length - System.getProperty("line.separator").length());   //获取请求消息
+        System.out.println("TimeServer Receive order : " + body + " ; the counter is : " + ++counter);
         String time = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+        time = time + System.getProperty("line.separator");
         ByteBuf resp = Unpooled.copiedBuffer(time.getBytes());
         ctx.write(resp);   //存入缓冲区
     }
@@ -25,8 +28,9 @@ public class TimeServerHandler extends ChannelHandlerAdapter {
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
         ctx.flush();   //将缓冲区的全部消息写入SocketChannel中进行发送
     }
+
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause){
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         ctx.close();   //发生异常时，释放资源
     }
 }
